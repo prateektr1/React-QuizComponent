980deb10aebeb9dbcfa685343d327c5a15354556
prateektr1 <prateektripathi63@gmail.com>
2019-03-31 10:40:07 +0530

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,7 @@ class QuizQuestion extends Component
 {
    handleClick(buttonText)
    {
-    this.props.clickHandler(this.props.buttonText)
+    
     if(buttonText===this.props.quiz_question.answer)
     {
         this.props.showNextQuestonHandler();
